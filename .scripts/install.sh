426591fb90fb50bdf77da442d7a502b31e4bc070
Gustavo Santos <gustavogustav54@gmail.com>
2026-05-13 10:05:58 -0300

    chore: do not copy commands

--- .scripts/install.sh
+++ .scripts/install.sh
@@ -5,7 +5,6 @@ PLUGIN_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 
 echo "Installing local Cursor files..."
 cp -R "$PLUGIN_DIR/skills/" "$HOME/.cursor/skills/"
-cp -R "$PLUGIN_DIR/commands/" "$HOME/.cursor/commands/"
 cp -R "$PLUGIN_DIR/agents/" "$HOME/.cursor/agents/"
 echo "Installing local Cursor plugin... OK"
 
